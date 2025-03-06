b1498045b602687343fef0ff5e0c05ea06f1d8aa
Ignacio Vázquez <vazquez.ignacio@gmail.com>
2025-03-06 19:39:53 -0300

    Add files via upload

---
+++ js/portafolio.js
@@ -0,0 +1,5 @@
+//2025-02-25_IV: Se definen acá las funciones que se utilizaran para darle vida a la página web Portafolio
+console.log("Javascript file was activated here!");
+
+/*El valor de document.compatMode en JavaScript mostrará si el documento está en quirks mode o no. Si su valor es "BackCompat", el documento está en quirks mode. Si no es así, tendrá el valor "CSS1Compat". */
+console.log(document.compatMode);
